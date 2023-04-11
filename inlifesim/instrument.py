bugfix for the exozodi calculation. fftshift was done along wavelength, which had to be removed. Image size for fft is now caluclated based on array geometry

inlifesim/instrument.py
@@ -1,10 +1,12 @@
 from typing import Union
 import multiprocessing as mp
 
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.special import jn, spherical_jn
 from scipy.fft import fft, fftfreq, fftshift, rfft
 import pandas as pd
+import xarray as xr
 
 from inlifesim.util import black_body, find_nearest_idx
 
@@ -70,6 +72,7 @@ class Instrument(object):
         # setting simulation parameters
         self.wl_bins = wl_bins
         self.wl_bin_widths = wl_bin_widths
+        # TODO: image size deprecated, remove
         self.image_size = image_size
         self.n_sampling_rot = n_sampling_rot
         self.pink_noise_co = pink_noise_co
@@ -258,7 +261,8 @@ class Instrument(object):
                                                        'instrumental',  # instrumental noise
                                                        'snr'  # signal to noise ratio
                                                        ],
-                                              index=[str(np.round(wl*1e6, 1)) for wl in self.wl_bins])
+                                              index=[str(np.round(wl*1e6, 1)) for wl in self.wl_bins]
+                                              )
 
         self.photon_rates_nchop = pd.DataFrame(columns=['signal',  # planet signal
                                                         'noise',  # overall noise contribution
@@ -290,7 +294,8 @@ class Instrument(object):
                                                         'instrumental',  # instrumental noise
                                                         'snr'  # signal to noise ratio
                                                         ],
-                                               index=[str(np.round(wl*1e6, 1)) for wl in self.wl_bins])
+                                               index=[str(np.round(wl*1e6, 1)) for wl in self.wl_bins]
+                                               )
 
         self.photon_rates_nchop['wl'] = self.wl_bins
         self.photon_rates_chop['wl'] = self.wl_bins
@@ -304,6 +309,12 @@ class Instrument(object):
 
         self.bl_x = np.array([(self.col_pos[:, 0] - self.col_pos[i, 0]) for i in range(self.num_a)])
         self.bl_y = np.array([(self.col_pos[:, 1] - self.col_pos[i, 1]) for i in range(self.num_a)])
+
+        # set image size to appropriate sampling
+        # TODO: Add scaling factor for image size, currently hardcoded to 2
+        self.image_size = (4 * np.pi * np.max(np.abs(np.concatenate((self.bl_x.flatten(), self.bl_y.flatten()))))
+                           / self.diameter_ap + 2) * 2
+        self.image_size = int(np.ceil(self.image_size / 2.) * 2)
 
         # TODO: Factor 2 or factor 1 here?
         self.omega = 1 * np.pi * (self.wl_bins/(2. * self.diameter_ap))**2
@@ -434,9 +445,12 @@ class Instrument(object):
         sampling_rate_rad = self.rad_pix
 
         # TODO: Check the transformation coefficients here
-        ez_fft = np.fft.fftshift(np.fft.fft2(flux_map_exozodi)) / 2
+        ez_fft = np.fft.fftshift(np.fft.fft2(flux_map_exozodi), axes=(-2, -1)) / 2
+        # ez_fft = np.fft.fft2(flux_map_exozodi) / 2
         # ez_fft = np.fft.fftshift(np.fft.fft2(flux_map_exozodi)) / np.pi
-        r_rad_fft = np.fft.fftshift(np.fft.fftfreq(self.image_size, sampling_rate_rad[:, np.newaxis]))
+        r_rad_fft = np.fft.fftshift(np.fft.fftfreq(self.image_size, sampling_rate_rad[:, np.newaxis]), axes=(-1))
+        # r_rad_fft = np.fft.fftfreq(self.image_size, sampling_rate_rad[:, np.newaxis])
+
 
         bl_x_fft = 2 * np.pi * self.bl_x[np.newaxis, :, :] / self.wl_bins[:, np.newaxis, np.newaxis]
         bl_y_fft = 2 * np.pi * self.bl_y[np.newaxis, :, :] / self.wl_bins[:, np.newaxis, np.newaxis]
@@ -453,6 +467,39 @@ class Instrument(object):
                                                         int(find_nearest_idx(r_rad_fft[k, :], bl_x_fft[k, i, j])),
                                                         int(find_nearest_idx(r_rad_fft[k, :], bl_y_fft[k, i, j]))
                                                  ])
+
+        # plot bl_x_pix and bl_y_pix over the ez_fft image for the wavelength bin 13, 14 & 15
+        # plt_wls = [12, 13, 14, 15, 16]
+        # fig, ax = plt.subplots(nrows=len(plt_wls), figsize=(2,2*len(plt_wls)))
+        # for i, wl in enumerate(plt_wls):
+        #     ax[i].imshow(np.log10(np.abs(ez_fft[wl, :, :])))
+        #     ax[i].scatter(bl_x_pix[wl, :, :], bl_y_pix[wl, :, :], s=1)
+        #     ax[i].set_title(f'Wavelength bin {wl}')
+        # fig.tight_layout()
+        # plt.show()
+
+        # plot the mean of bl_y_pix for each wavelenght bin
+        # plt.plot(self.wl_bins, np.mean(bl_y_pix, axis=(1, 2)))
+        # plt.show()
+
+        # plot the mean of self.b_ez for each wavelenght bin and compare it to the mean of bl_y_pix
+        # plt.plot(np.mean(np.abs(bl_y_pix - np.mean(bl_y_pix)), axis=(1, 2)), label='bl_y_pix')
+        # plt.plot(np.mean(self.b_ez*10, axis=(1, 2)), label='b_ez')
+        # plt.xlabel('Wavelength bin')
+        # plt.ylabel('Mean of ...')
+        # plt.legend()
+        # plt.show()
+
+        # plot a grid of all ez_fft images
+        # fig, ax = plt.subplots(nrows=5, ncols=5, figsize=(10, 10))
+        # for i in range(5):
+        #     for j in range(5):
+        #         ax[i, j].imshow(np.log10(np.abs(ez_fft[i*5+j, :, :])))
+        #         ax[i, j].set_title(f'Wavelength bin {i*5+j}')
+        # fig.tight_layout()
+        # plt.show()
+        #
+        # a=1
 
     def response(self) -> None:
         theta_x = np.linspace(-1e-6, 1e-6, 200)[np.newaxis, :]