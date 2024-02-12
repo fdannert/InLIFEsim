dcdcd59f1fa45a0b6db7ed102b1fdd9bf0b5cfac
fdannert <62441325+fdannert@users.noreply.github.com>
2024-02-12 10:59:01 +0100

    tools for testing and plotting distributions; first implementation of iterative modified bessel function (PDF only)

--- inlifesim/statistics.py
+++ inlifesim/statistics.py
@@ -1,4 +1,10 @@
+from typing import Union
+
 import numpy as np
+from scipy.special import kv, gamma
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+
 from inlifesim.util import freq2temp_fft, dict_sumover
 
 
@@ -261,3 +267,284 @@ def draw_fourier_noise(psd: np.ndarray,
                       total_time=t_rot)
 
     return x, x_ft
+
+class imb(stats.rv_continuous):
+    def _pdf(self, x, n):
+        pdf = ((2 ** (0.5 * (1 - n)) * np.abs(x) ** (0.5 * (n-1)) * kv(0.5 * (n - 1), np.abs(x)))
+               / (np.sqrt(np.pi) * gamma(n / 2)))
+        return pdf
+
+
+def get_qq(data: np.ndarray,
+           loc: float,
+           scale: float,
+           mode: str,
+           nconv: Union[float, None] = None,
+           siglim: float = 7,
+           n_eval: int = 100):
+    """
+    A function to create quantile-quantile values that are evenly spaces along
+    the theoretical quantiles. Implemented for a normal and an iterative
+    modified bessel distribution.
+
+    Args:
+        data: np.ndarray
+            The data set for which the Q-Q plot is to be calculated.
+        loc: float
+            The location parameter of the distribution.
+        scale: float
+            The scale parameter of the distribution.
+        mode: str
+            The mode of the distribution. Either 'normal' for a Gaussian
+            distribution or 'imb' for an iterative modified bessel
+            distribution.
+        nconv: float, optional
+            The number of convolution steps for the iterative modified bessel
+            distribution. If the mode is 'normal', this parameter is ignored.
+        siglim: float, optional
+            The limit of up to how many standard deviations away from 0 the
+            PDFs are evaluated. The default is 7.
+        n_eval: int, optional
+            The number of evaluation points for the Q-Q plot. The default is
+            100.
+
+    Returns:
+        q: dict
+            The theoretical and sample quantiles.
+        q_good: dict
+            The theoretical and sample quantiles for a purely data based
+            calculation.
+        q_fail: dict
+            The theoretical and sample quantiles for the faulty extrapolation.
+            Here, the numerical evaluation of the ppf of the distribution
+            failed and is replaced by the native theoretical quantiles. Due to
+            the discrete nature of the data, this can lead to some imprecision
+            in the $q_sample$ values for this regime.
+    """
+    if mode == 'imb' and nconv is None:
+        raise ValueError('nconv must be specified for mode "imb"')
+
+    # data needs to be normalized
+    data /= np.std(data)
+
+    # start with an equally spaced grid of quantiles
+    q_theo_native = np.linspace(-siglim, siglim, n_eval)[1:-1]
+
+    # evaluate the p_values of the theoretical quantiles
+    if mode == 'normal':
+        dist = stats.norm(loc=loc, scale=scale)
+        p_theo_native = dist.cdf(q_theo_native)
+    elif mode == 'imb':
+        dist = imb()
+        p_theo_native = dist.cdf(q_theo_native, loc=loc, scale=scale, n=nconv)
+    else:
+        raise ValueError('Mode not recognized')
+
+    # snap the theoretical p_values to values that actually appear in the
+    # sample. This is done to avoid discretization errors in the sample
+    p_sample = np.round(p_theo_native * len(data)).astype(int)
+    p_theo = p_sample / len(data)
+
+    # remove the p_values that are 0 or 1, as the ppf is not defined for these
+    # values.
+    mask = np.logical_or(p_theo == 0, p_theo == 1)
+    p_theo = p_theo[~mask]
+    p_sample = p_sample[~mask]
+
+    # get the data q-values by sorting the data and extracting at the selected
+    # p_value positions
+    data_sorted = np.sort(data)
+    q_sample = data_sorted[p_sample]
+
+    # get the theoretical q-values by evaluating the ppf of the distribution
+    if mode == 'normal':
+        q_theo = dist.ppf(p_theo)
+    elif mode == 'imb':
+        q_theo = dist.ppf(p_theo, loc=loc, scale=scale, n=nconv)
+    else:
+        raise ValueError('Mode not recognized')
+
+    # in some cases the ppf fails to evaluate. It then returns 0, for which
+    # it is replaced by the non-gridded theoretical quantiles.
+    mask_q_theo_fail = (q_theo == 0.)
+    q_good = {'theo': q_theo[~mask_q_theo_fail],
+              'sample': q_sample[~mask_q_theo_fail]}
+
+    q_fail = {'theo': q_theo_native[~mask][mask_q_theo_fail],
+              'sample': data_sorted[p_sample[mask_q_theo_fail]]}
+
+    q = {'theo': np.append(q_good['theo'], q_fail['theo']),
+         'sample': np.append(q_good['sample'], q_fail['sample'])}
+
+    ss_res = np.sum((q['sample'] - q['theo']) ** 2)
+    ss_tot = np.sum((q['sample'] - np.mean(q['sample'])) ** 2)
+    r_2 = 1 - ss_res / ss_tot
+
+    return q, q_good, q_fail, r_2
+
+
+def test_dist(data: np.ndarray,
+              mode: str,
+              loc: float,
+              scale: float,
+              nconv: Union[float, None] = None,
+              fit: bool = True,
+              plot: bool = True,
+              siglim: float = 7,
+              n_eval_pdf: int = 1000,
+              n_eval_qq: int = 100,
+              y_log: bool = True,
+              pdf_only: bool = False,
+              color: str = 'tab:orange'):
+    """
+    A function to test the distribution of data against either a normal or an
+    iterative modified bessel distribution. It can be used to check the fit of
+    the PDF to the data histogram, as well as the Q-Q plot and the R^2 value.
+
+    Args:
+        data: np.ndarray
+            The data set. Needs to be 1-D.
+        mode: str
+            The mode of the distribution. Either 'normal' for a Gaussian
+            distribution or 'imb' for an iterative modified bessel
+            distribution.
+        loc: float
+            The location parameter of the distribution.
+        scale: float
+            The scale parameter of the distribution.
+        nconv: float, optional
+            The number of convolution steps for the iterative modified bessel
+            distribution. If the mode is 'normal', this parameter is ignored.
+        fit: bool, optional
+            If True, the function will fit the slope of the Q-Q plot. The
+            default is True.
+        plot: bool, optional
+            If True, the function will create a plot of the Q-Q plot and the
+            PDF. The default is True.
+        siglim: float, optional
+            The limit of up to how many standard deviations away from 0 the
+            PDFs are evaluated. The default is 7.
+        n_eval_pdf: int, optional
+            The number of evaluation points for the PDF. The default is 1000.
+        n_eval_qq: int, optional
+            The number of evaluation points for the Q-Q plot. The default is
+            100.
+        y_log: bool, optional
+            If True, the y-axis of the PDF will be logarithmic. The default is
+            True.
+        pdf_only: bool, optional
+            If True, the function will only create the PDF plot. The default is
+            False.
+        color: str, optional
+            The color of the Q-Q plot fit line. The default is 'tab:orange'.
+
+    Returns:
+        slope: float
+            The slope of the Q-Q plot fit line.
+        r_2: float
+            The R^2 value of the Q-Q plot.
+    """
+
+    if nconv is None and mode == 'imb':
+        raise ValueError('nconv must be specified for mode "imb"')
+
+    if plot:
+        fig, ax = plt.subplots(ncols=2, figsize=(8, 1.05 * 8 / 2), dpi=200)
+
+        q_theo = np.linspace(-siglim, siglim, n_eval_pdf)
+
+        hist = ax[0].hist(data / np.std(data),
+                          bins=200,
+                          density=True,
+                          color='k',
+                          histtype=u'step',
+                          label='Data')
+
+        if mode == 'normal':
+            pdf_guess = stats.norm.pdf(x=q_theo, loc=loc, scale=scale)
+        elif mode == 'imb':
+            pdf_guess = imb().pdf(x=q_theo, loc=loc, scale=scale, n=nconv)
+        else:
+            raise ValueError('Mode not recognized')
+
+        ax[0].plot(q_theo, pdf_guess,
+                   color='k', ls='--', lw=0.75, alpha=0.5,
+                   label='Distribution guess (hist)')
+
+        if y_log:
+            ax[0].set_yscale('log')
+        ax[0].legend(loc='lower center')
+        ax[0].set_ylim(np.sort(np.unique(hist[0]))[1] * 0.5,
+                       np.max(np.concatenate((hist[0], pdf_guess))) * 1.5)
+
+    if not pdf_only:
+        q, q_good, q_fail, r_2 = get_qq(data=data,
+                                        loc=loc,
+                                        scale=scale,
+                                        mode=mode,
+                                        nconv=nconv,
+                                        siglim=siglim,
+                                        n_eval=n_eval_qq)
+
+        if fit:
+            slope, _, _, _, _ = stats.linregress(x=q['theo'], y=q['sample'])
+
+        if plot:
+            if fit:
+                q, q_good, q_fail, r_2 = get_qq(data=data,
+                                                loc=loc,
+                                                scale=scale * slope,
+                                                mode=mode,
+                                                nconv=nconv,
+                                                siglim=siglim,
+                                                n_eval=n_eval_qq)
+
+            lim = np.max(np.concatenate((np.abs(q['theo']),
+                                         np.abs(q['sample']))))
+            plt.scatter(q_good['theo'], q_good['sample'],
+                        color='k', alpha=1, label='pure data')
+            ax[1].scatter(q_fail['theo'], q_fail['sample'],
+                          alpha=1, marker='o', fc='w',
+                          label='potential faulty \nextrapolation',
+                          color='gray')
+
+            if fit:
+                ax[1].plot([-lim, lim], [-lim, lim], color=color, ls='-')
+                ax[1].plot([-lim, lim], [-lim / slope, lim / slope],
+                           color='k', alpha=0.5, lw=0.75, ls='--')
+            else:
+                ax[1].plot([-lim, lim], [-lim, lim],
+                           color='k', lw=0.75, ls='--')
+
+            ax[1].text(0.95, 0.05, f'$R^2 = {r_2:.5f}$',
+                       transform=ax[1].transAxes, va='bottom', ha='right')
+
+            ax[1].set_xlabel('Theoretical quantiles')
+            ax[1].set_ylabel('Sample quantiles')
+            ax[1].set_xlim(-lim * 1.1, lim * 1.1)
+            ax[1].set_ylim(-lim * 1.1, lim * 1.1)
+            ax[1].legend()
+
+            if fit:
+                if mode == 'normal':
+                    pdf_fit = stats.norm.pdf(x=q_theo,
+                                             loc=loc,
+                                             scale=scale * slope)
+                elif mode == 'imb':
+                    pdf_fit = imb().pdf(x=q_theo,
+                                           loc=loc,
+                                           scale=scale * slope,
+                                           n=nconv)
+                else:
+                    raise ValueError('Mode not recognized')
+                ax[0].plot(q_theo, pdf_fit,
+                           color=color, lw=0.75,
+                           label='Distribution fit (R^2)')
+
+            plt.tight_layout()
+            plt.show()
+
+        if fit:
+            return slope, r_2
+        else:
+            return r_2
