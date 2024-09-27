second order noise gives incorrect values, bugfixing

inlifesim/observatory.py
@@ -65,6 +65,7 @@ class Instrument(object):
                  verbose: bool = False,
                  draw_samples: bool = False,
                  get_single_bracewell: bool = False,
+                 instrumental_source: Union[None, str] = None,
                  ):
         '''
         Observatory instance to calculate instrumental noise. TODO: add example
@@ -195,6 +196,7 @@ class Instrument(object):
         self.n_draws_per_run = n_draws_per_run
         self.time_samples_return_values = time_series_return_values
         self.get_single_bracewell = get_single_bracewell
+        self.instrumental_source = instrumental_source
 
         if self.draw_samples and (n_draws is None):
             raise ValueError('Sample size must be set in sampling mode')
@@ -483,12 +485,12 @@ class Instrument(object):
                                       for k in self.hess_star.keys()})
 
             self.grad_n_coeff_chop.append({k: self.grad_star_chop[k][i]
-                                         + self.grad_ez_chop[k][i]
-                                         + self.grad_lz[k][i]
-                                      for k in self.grad_star.keys()})
+                                              + self.grad_ez_chop[k][i]
+                                              + self.grad_lz[k][i]
+                                           for k in self.grad_star.keys()})
             self.hess_n_coeff_chop.append({k: self.hess_star_chop[k][i]
-                                         + self.hess_ez_chop[k][i]
-                                      for k in self.hess_star.keys()})
+                                              + self.hess_ez_chop[k][i]
+                                           for k in self.hess_star.keys()})
 
     def build_gradient_hessian(
             self,
@@ -569,15 +571,11 @@ class Instrument(object):
         # prepare variable dictionary to send to multiprocessing workers
         mp_args = []
         for i in range(self.wl_bins.shape[0]):
-            mp_args.append({
+            arg = {
                 'A': self.A,
                 'wl': self.wl_bins[i],
                 'num_a': self.num_a,
                 'planet_template_chop': self.planet_template_chop[i, :],
-                'grad_n_coeff': self.grad_n_coeff[i],
-                'hess_n_coeff': self.hess_n_coeff[i],
-                'grad_n_coeff_chop': self.grad_n_coeff_chop[i],
-                'hess_n_coeff_chop': self.hess_n_coeff_chop[i],
                 'rms_mode': self.rms_mode,
                 'n_sampling_max': self.n_sampling_max,
                 'harmonic_number_n_cutoff':
@@ -589,7 +587,45 @@ class Instrument(object):
                 'flux_star': self.flux_star[i],
                 'n_rot': self.n_rot,
                 'hyperrot_noise': self.hyperrot_noise
-            })
+            }
+
+            if self.instrumental_source is None:
+                arg['grad_n_coeff'] = self.grad_n_coeff[i]
+                arg['hess_n_coeff'] = self.hess_n_coeff[i]
+                arg['grad_n_coeff_chop'] = self.grad_n_coeff_chop[i]
+                arg['hess_n_coeff_chop'] = self.hess_n_coeff_chop[i]
+
+            elif self.instrumental_source == 'star':
+                arg['grad_n_coeff'] = {k: self.grad_star[k][i]
+                                       for k in self.grad_star.keys()}
+                arg['hess_n_coeff'] = {k: self.hess_star[k][i]
+                                       for k in self.hess_star.keys()}
+                arg['grad_n_coeff_chop'] = {
+                    k: self.grad_star_chop[k][i]
+                    for k in self.grad_star_chop.keys()
+                }
+                arg['hess_n_coeff_chop'] = {
+                    k: self.hess_star_chop[k][i]
+                    for k in self.hess_star_chop.keys()
+                }
+
+            elif self.instrumental_source == 'ez':
+                arg['grad_n_coeff'] = {k: self.grad_ez[k][i]
+                                       for k in self.grad_ez.keys()}
+                arg['hess_n_coeff'] = {k: self.hess_ez[k][i]
+                                       for k in self.hess_ez.keys()}
+                arg['grad_n_coeff_chop'] = {
+                    k: self.grad_ez_chop[k][i]
+                    for k in self.grad_ez_chop.keys()
+                }
+                arg['hess_n_coeff_chop'] = {
+                    k: self.hess_ez_chop[k][i]
+                    for k in self.hess_ez_chop.keys()
+                }
+
+            else:
+                raise ValueError('Instrumental source not recognized')
+            mp_args.append(arg)
         if self.n_cpu == 1:
             res = []
             for i in range(self.wl_bins.shape[0]):
@@ -617,7 +653,7 @@ class Instrument(object):
         self.photon_rates_chop['pn'] = np.sqrt(
             (self.photon_rates_chop['pn_sgl'] ** 2
             # the sqaure is missing on purpose, see perturbation.py
-             + self.photon_rates_chop['pn_snfl']
+             + self.photon_rates_chop['pn_snfl'] ** 2
              + self.photon_rates_chop['pn_lz'] ** 2
              + self.photon_rates_chop['pn_ez'] ** 2
              + self.photon_rates_chop['pn_pa'] ** 2).astype(float)
@@ -631,7 +667,7 @@ class Instrument(object):
 
         self.photon_rates_chop['noise'] = np.sqrt(
             (self.photon_rates_chop['pn'] ** 2
-             + self.photon_rates_chop['sn']).astype(float)
+             + self.photon_rates_chop['sn'] ** 2).astype(float)
         )
 
         self.photon_rates_chop['snr'] = (self.photon_rates_chop['signal']

inlifesim/perturbation.py
@@ -337,6 +337,9 @@ def sys_noise_chop(mp_arg) -> dict:
         hyperrot_noise=hyperrot_noise
     )
 
+    # d_a_psd /= 19.71
+    # d_phi_psd /= 19.71
+
     # noise contribution
     noise_chop = {'wl': wl}
 
@@ -349,8 +352,8 @@ def sys_noise_chop(mp_arg) -> dict:
 
     d_phi_j_hat_2_chop = np.full(
         shape=num_a,
-        fill_value=(np.sum(d_phi_psd * np.abs(planet_template_c_fft) ** 2)
-                    / t_total ** 5 * len(planet_template_chop) ** 2)
+        fill_value=(np.sum(d_phi_psd[int(len(d_phi_psd)/2):] * np.abs(planet_template_c_fft[int(len(planet_template_c_fft)/2):]) ** 2)
+                    / t_total ** 5 * (len(planet_template_chop)/2) ** 2)
     )
 
     d_a_j_hat_2_chop = np.full(
@@ -368,38 +371,43 @@ def sys_noise_chop(mp_arg) -> dict:
     # poisson noise from null floor perturbation
     dn_null_floor = np.sum(
         np.array([((hess_n_coeff['aa'][j, j]
-                    - hess_n_coeff_chop['aa'][j, j])
+                    #- hess_n_coeff_chop['aa'][j, j]
+                    )
                    * d_a_j_hat_2_chop[j]
                    + (hess_n_coeff['phiphi'][j, j]
-                      - hess_n_coeff_chop['phiphi'][j, j])
+                      #- hess_n_coeff_chop['phiphi'][j, j]
+                      )
                    * d_phi_j_hat_2_chop[j])
                   for j in range(num_a)])
     )
-    noise_chop['pn_snfl'] = dn_null_floor * t_total
-
-    if (n_rot is None) or (n_rot == 1):
-        fact = 1
-    else:
-        fact = 2
+    noise_chop['pn_snfl'] = np.sqrt(dn_null_floor * t_total)
 
     # second order dadphi
+    # d_a_d_phi_j_hat_2_chop = np.full(
+    #     shape=(num_a, num_a),
+    #     fill_value=np.sum(
+    #         np.convolve(d_a_psd[0], d_phi_psd, mode='same')
+    #         * np.abs(planet_template_c_fft)**2
+    #     ) * len(planet_template_chop) ** 4 / t_total ** 8 / n_rot
+    # )
+
     d_a_d_phi_j_hat_2_chop = np.full(
         shape=(num_a, num_a),
         fill_value=np.sum(
-            np.convolve(d_a_psd[0], d_phi_psd, mode='same') * fact
-            * np.abs(planet_template_c_fft)**2
-        ) * len(planet_template_chop) ** 4 / t_total ** 8
+            np.convolve(d_a_psd[0][int(len(d_a_psd[0]) / 2):], d_phi_psd[int(len(d_phi_psd)/2):], mode='same')
+            * np.abs(planet_template_c_fft[int(len(planet_template_c_fft)/2):])**2
+        ) * (len(planet_template_chop)/2) ** 4 / t_total ** 8 / n_rot * 2
     )
 
     noise_chop['sn_so_aphi'] = np.sum((hess_n_coeff['aphi']
                                        - hess_n_coeff_chop['aphi']) ** 2
-                                      * d_a_d_phi_j_hat_2_chop * t_total ** 2)
+                                      * 2 * d_a_d_phi_j_hat_2_chop * t_total ** 2)
 
     noise_chop['sn_fo'] = noise_chop['sn_fo_phi']
     noise_chop['sn_so'] = noise_chop['sn_so_aphi']
 
-    noise_chop['sn'] = np.sqrt(noise_chop['sn_fo_phi'] ** 2
-                               + noise_chop['sn_so_aphi'] ** 2)
+    noise_chop['sn'] = np.sqrt(noise_chop['sn_fo_phi']
+                               + noise_chop['sn_so_aphi'])
 
     debug_sys_noise_chop(d_phi_b_2=d_phi_b_2,
                          planet_template_c_fft=planet_template_c_fft,

inlifesim/signal.py
@@ -136,7 +136,9 @@ def planet_signal(separation_planet: float,
     # removal of even components and DC
     nfft_odd = nfft
     if not simultaneous_chopping:
-        nfft_odd[:, ::2] = 0
+        nfft_odd[:, int(nfft_odd.shape[1] / 2)::2] = 0
+        nfft_odd[:, :int(nfft_odd.shape[1] / 2)] = np.flip(
+            nfft_odd[:, int(nfft_odd.shape[1] / 2 + 1):], axis=1)
 
     # transform back into time domain
     planet_template_nchop = freq2temp_ft(nfft_odd, t_rot)
@@ -177,7 +179,10 @@ def planet_signal(separation_planet: float,
     # creation of template function
     # removal of even components and DC
     nfft_odd_chop = nfft_chop
-    nfft_odd_chop[:, ::2] = 0
+    nfft_odd_chop[:, int(nfft_odd_chop.shape[1] / 2)::2] = 0
+    nfft_odd_chop[:, :int(nfft_odd_chop.shape[1] / 2)] = np.flip(
+        nfft_odd_chop[:, int(nfft_odd_chop.shape[1] / 2 + 1):], axis=1)
+
 
     # transform back into time domain
     planet_template_chop = freq2temp_ft(nfft_odd_chop, t_rot)

inlifesim/sources.py
@@ -230,7 +230,7 @@ def create_exozodi(wl_bins: np.ndarray,
     # Sigma_{m,0} from Kennedy+2015 (doi:10.1088/0067-0049/216/2/23)
     sigma_zero = 7.11889e-8
 
-    # identify all pixels where the radius is larges than the inner radius by
+    # identify all pixels where the radius is larger than the inner radius by
     # Kennedy+2015
     r_cond = ((r_au >= r_in)
               & (r_au <= image_size / 2 * au_pix[:, np.newaxis, np.newaxis]))

inlifesim/spectra.py
@@ -127,15 +127,15 @@ def create_pink_psd(t_total: float,
             hyperrot_noise = 'zero'
         psd = (2 * rms ** 2 * t_total ** 3 / (2 * n_sampling_max) ** 2
                / harmonic_number_n_cutoff
-               / np.arange(n_rot, n_sampling_max))
+               / np.arange(n_rot-1, n_sampling_max) * n_rot)
         if hyperrot_noise == 'zero':
-            hyper_psd=np.zeros(n_rot)
+            hyper_psd=np.zeros(n_rot-1)
         elif hyperrot_noise == 'max':
-            hyper_psd = np.ones(n_rot) * np.max(psd)
+            hyper_psd = np.ones(n_rot-1) * np.max(psd)
         elif hyperrot_noise == '1/f_r':
-            hyper_psd = 1 / np.arange(n_rot+1, 1, -1) * np.max(psd)
+            hyper_psd = 1 / np.arange(n_rot, 1, -1) * np.max(psd)
         elif hyperrot_noise == '1/f':
-            hyper_psd = 1 / np.arange(1, n_rot+1) * np.max(psd) * (n_rot+1)
+            hyper_psd = 1 / np.arange(1, n_rot) * np.max(psd) * (n_rot)
         else:
             raise ValueError('Hyperrotational noise mode not recognized')
 